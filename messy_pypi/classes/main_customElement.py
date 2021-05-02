ea7c3a485abf5703cf5db2cd57b85af404846abb
Maxim-Costa <mxmcosta@gmail.com>
2021-05-02 22:41:06 +0200

    add game minesweeper

--- messy_pypi/classes/main_customElement.py
+++ messy_pypi/classes/main_customElement.py
@@ -1,9 +1,20 @@
-class list(List):
+class List(list):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    def rma(self,elt):
+
+    def rma(self, elt):
         while elt in self:
             self.remove(elt)
-    def rm(self,elt):
+
+    def rm(self, elt):
         if elt in self:
             self.remove(elt)
+
+    def indexExist(self, index):
+        if 0 <= index < len(self):
+            return True
+        return False
+
+    @property
+    def length(self):
+        return len(self)
