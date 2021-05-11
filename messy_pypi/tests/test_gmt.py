aae96cbc40b4d6a9ec6774056ffdf8ccb557bdb3
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2021-05-11 10:52:24 +0200

    Test fix et add Arkanyota TODO at readme

--- messy_pypi/tests/test_gmt.py
+++ messy_pypi/tests/test_gmt.py
@@ -1,6 +1,6 @@
 import sys
 
 sys.path.append('../')
-from functions.main_gamemakerterminal import GameMakerTerminal
+from main_gamemakerterminal import GameMakerTerminal
 
 GameMakerTerminal("map.txt","config.txt")
